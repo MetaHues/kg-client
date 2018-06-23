3f5ac1a78e01773411ff3446628dd142d5881046
jonathan earl <earl.jonathan@gmail.com>
2018-06-22 19:44:21 -0700

    restyle for mobile windows

--- src/component/Navigator.jsx
+++ src/component/Navigator.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 
 // style
 import '../css/Navigator.css'
@@ -8,8 +8,9 @@ class Navigator extends Component {
         return (
             <div className="Navigator">
                 <nav>
-                    <div className="logo"><a href=""><i className="fa fa-diamond"/> | KittyGlitter</a></div>
-                    <div className="search">
+                    <div className="logo">
+                    <a href=""><span className='logo-small'><i className="fa fa-diamond"/> | KG</span><span className='logo-large'> KittyGlitter</span></a></div>
+                    <div className="search-large">
                         <div className="searchBoarder">
                             <i className="fa fa-search"/>
                             <input type="text" placeholder="Search"/>
@@ -17,15 +18,21 @@ class Navigator extends Component {
                     </div>
                     <div className="subnav">
                         <ul>
-                            <a href=""><li><i className="fa fa-diamond"/></li></a>
+                            <a href=""><li><i className="fa fa-coffee"/></li></a>
                             <a href=""><li><i className="fa fa-heart-o"/></li></a>
-                            <a href=""><li> <i className="fa fa-user-o"/></li></a>
+                            <a href=""><li><i className="fa fa-user-o"/></li></a>
                         </ul>
                     </div>
                 </nav>
+                <div className="search-small">
+                        <div className="searchBoarder">
+                            <i className="fa fa-search"/>
+                            <input type="text" placeholder="Search"/>
+                        </div>
+                    </div>
             </div>
-        );
+        )
     }
 }
 
-export default Navigator;
+export default Navigator
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './component/App';
 import registerServiceWorker from './registerServiceWorker';
-import 'font-awesome/css/font-awesome.min.css';
+import 'font-awesome/css/font-awesome.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
