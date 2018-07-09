dbe1aedb83e66c7aeb5693a9d94a14464e3f6f22
jonathan earl <earl.jonathan@gmail.com>
2018-07-09 14:17:15 -0700

    fix binding issue with pulling out onClick submit createpost

--- src/component/container/CreatePost.jsx
+++ src/component/container/CreatePost.jsx
@@ -1,41 +1,37 @@
 import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
 import axios from 'axios'
 
 class CreatePost extends Component {
-    componentDidMount() {
-        console.dir(this.props)
-        const { history } = this.props
-        console.log()
-        document.querySelector('#post_submit').onclick = function() {
-            let newPost = {
-                media: {
-                    img: document.querySelector('#post_img').value
-                },
-                msg: document.querySelector('#post_comment').value,
-                likes: 0,
-                comments: []
-            }
-            console.log(newPost)
-            console.log(document.querySelector('#post_img'))
-            axios.post('/api/post', newPost)
-            .then((res) => {
-                console.log('res')
-                console.log(res)
-                history.push(`/post/${res.data._id}`)
-            })
-            .catch(err => {
-                console.log(err)
-            })
+    SubmitPost = (e) => {
+        e.preventDefault()
+
+        let newPost = {
+            media: {
+                img: document.querySelector('#post_img').value
+            },
+            msg: document.querySelector('#post_comment').value,
+            likes: 0,
+            comments: []
         }
+        axios.post('/api/post', newPost)
+        .then((res) => {
+            console.log('res')
+            console.log(res)
+            this.props.history.push(`/post/${res.data._id}`)
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
 
     render() {
         return (
             <div className='CreatePost'>
-                <input type='text' id='post_img' name='post_img' placeholder='Image URL' />
-                <input type='text' id='post_comment' name='post_comment' placeholder='Comment' />
-                <button id='post_submit'>Submit</button>
+                <form action='/api/post' method='post'>
+                    <input type='text' id='post_img' name='post_img' placeholder='Image URL' />
+                    <input type='text' id='post_comment' name='post_comment' placeholder='Comment' />
+                    <button type='button' onClick={this.SubmitPost} >Submit</button>
+                </form>
             </div>
         )
     }
