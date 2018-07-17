172725368cec618c33dd194345cfe3e3b4d5cd13
jonathan earl <earl.jonathan@gmail.com>
2018-07-16 19:14:28 -0700

    add img preview, (this should use pure JS avoid dom interaction)

--- src/component/container/CreatePost.jsx
+++ src/component/container/CreatePost.jsx
@@ -4,8 +4,13 @@ import axios from 'axios'
 // components
 import NavigatorMobile from '../navigation/NavigatorMobile'
 
+// styles
+import '../../css/CreatePost.css'
+
 class CreatePost extends Component {
-    SubmitPost = (e) => {
+
+
+    submitPost = (e) => {
         e.preventDefault()
         console.log(this.props)
         let newPost = {
@@ -25,14 +30,27 @@ class CreatePost extends Component {
         })
     }
 
+    setImgUrl() {
+        let src = document.querySelector('#post_img').value
+        let img = document.querySelector('.CreatePost__preview-img')
+        console.log('img', img)
+        img.src = src
+    }
+
     render() {
         return (
             <div className='CreatePost'>
-                <form action='/api/post' method='post'>
-                    <input type='text' id='post_img' name='post_img' placeholder='Image URL' />
-                    <input type='text' id='post_comment' name='post_comment' placeholder='Comment' />
-                    <button type='button' onClick={this.SubmitPost} >Submit</button>
-                </form>
+                <div className="CreatePost__container">
+                    <h1 className='CreatePost__title'>Post a picture</h1>
+                    <div className="CreatePost__preview-img-container">
+                        <img src="" alt="" className="CreatePost__preview-img"/>
+                    </div>
+                    <form action='/api/post' method='post'>
+                        <input type='text' id='post_img' name='post_img' placeholder='Image URL' onChange={this.setImgUrl} />
+                        <input type='text' id='post_comment' name='post_comment' placeholder='Comment' />
+                        <button type='button' onClick={this.submitPost} >Submit</button>
+                    </form>
+                </div>
                 <NavigatorMobile/>
             </div>
         )
