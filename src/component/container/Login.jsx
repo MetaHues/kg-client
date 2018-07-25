32f68acd50bf33bb91bf74be12c86c1b9a856028
jonathan earl <earl.jonathan@gmail.com>
2018-07-25 15:14:18 -0700

    add small margin to larger screensizes so that last post is not stuck on screen bottom, show edit button ONL in profile

--- src/component/container/Login.jsx
+++ src/component/container/Login.jsx
@@ -7,13 +7,15 @@ import '../../css/Login.css'
 class Login extends Component {
     render() {
         return (
-            <div className="Login">
-                <div className="container border">
-                    <div className="logo">
+            <div className="login">
+                <div className="login__container login__container--border">
+                    <div className="login__logo logo">
                         <Link className='logo-link' to='/'>KittyGlitter</Link>
                     </div>
-                    <div className="login-intro-gif">
-                        <img src="https://media.giphy.com/media/3o85xAfFLHv8scEHsc/giphy.gif" alt=""/>
+                    <div className='login__img-container'>
+                        <Link to='/'>
+                            <img className='login__gif' src="https://media.giphy.com/media/3o85xAfFLHv8scEHsc/giphy.gif" alt=""/>
+                        </Link>
                     </div>
                     <div className="auth_button">
                         <a href="/auth/facebook" alt="facebook login button" >
