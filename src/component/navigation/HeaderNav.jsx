da470dee8a779a97c61cb841c7bda8f97e8211b2
jonathan earl <earl.jonathan@gmail.com>
2018-08-28 01:02:06 -0700

    change header nav class names to relevent

--- src/component/navigation/HeaderNav.jsx
+++ src/component/navigation/HeaderNav.jsx
@@ -6,14 +6,14 @@ import '../../css/HeaderNav.css'
 
 export default function HeaderNav() {
     return (
-        <div className='page-header_nav'>
-            <nav className='page-header_nav-list-container'>
-                <ul className='page-header_nav-list'>
-                    <Link className='page-header_nav-list-item' to={'/home'}><li><i className='fa fa-home'/></li></Link>
-                    <Link className='page-header_nav-list-item' to={'/explore'}><li><i className='fa fa-search'/></li></Link>
-                    <Link className='page-header_nav-list-item' to={'/createpost'}><li><i className='fa fa-camera-retro'/></li></Link>
-                    <Link className='page-header_nav-list-item' to={'/likes'}><li><i className="fa fa-diamond"/></li></Link>
-                    <Link className='page-header_nav-list-item' to={'/profile'}><li><i className="fa fa-user-o"/></li></Link>                        
+        <div className='header-nav'>
+            <nav className='header-nav-list-container'>
+                <ul className='header-nav-list'>
+                    <Link className='header-nav-list-item' to={'/home'}><li><i className='fa fa-home'/></li></Link>
+                    <Link className='header-nav-list-item' to={'/explore'}><li><i className='fa fa-search'/></li></Link>
+                    <Link className='header-nav-list-item' to={'/createpost'}><li><i className='fa fa-camera-retro'/></li></Link>
+                    <Link className='header-nav-list-item' to={'/likes'}><li><i className="fa fa-diamond"/></li></Link>
+                    <Link className='header-nav-list-item' to={'/profile'}><li><i className="fa fa-user-o"/></li></Link>                        
                 </ul>
             </nav>
         </div>
