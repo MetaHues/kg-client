dcf03211bfff349e84b0b6ebc8a08bfcf46e1150
jonathan earl <earl.jonathan@gmail.com>
2018-09-14 13:19:16 -0700

    fix showing initial user msg on post

--- src/component/partial/CommentArea.jsx
+++ src/component/partial/CommentArea.jsx
@@ -4,6 +4,16 @@ import React, {Component} from 'react'
 import Comment from './Comment'
 
 class CommentArea extends Component {
+    renderUserCommentOnly() {
+        return (
+            <div className="comment-area section">
+                <div className="comment-area__wrapper">
+                    <Comment userName={this.props.user.name} userComment={this.props.post.msg} />
+                </div>
+            </div>
+        )
+    }
+
     renderComments() {
         let comments = this.props.comments.map(comment => {
             return (<li key={comment._id}><Comment  userName={comment.userName} userComment={comment.msg} /></li>)
@@ -12,7 +22,14 @@ class CommentArea extends Component {
     }
 
     render() {
-        if(!this.props.post || !this.props.user || this.props.comments.length === 0) return null
+        if(!this.props.post || !this.props.user) {
+            return null
+        }
+
+        if(this.props.comments.length === 0) {
+            return this.renderUserCommentOnly()
+        }
+
         return (
             <div className="comment-area section">
                 <div className="comment-area__wrapper">
